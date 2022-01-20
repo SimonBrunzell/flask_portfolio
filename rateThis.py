7272c779e40b707319b52a616643778cb29f8c41
simon <simoncbrunzell@gmail.com>
2022-01-20 12:57:47 -0800

    ok finally got rateThis to properly check for strings yayy

--- rateThis.py
+++ rateThis.py
@@ -7,9 +7,26 @@ try:
 except ValueError:
     error_Message = " {} is not an integer please only input integers "
     print(error_Message.format(rating1))
-    rating1 = input("From a scale of 1-5 how cute is this picture?: ")
-while x > 5 or x < 1:
-    rating1 = input("please follow directions...From a scale of 1-5 how cute is this picture?: ")
+    rating1 = input("From a scale of 1-5 how cute is this picture: ")
+
+    if rating1 == "1":
+        x = int(rating1)
+    if rating1 == "2":
+        x = int(rating1)
+    if rating1 == "3":
+        x = int(rating1)
+    if rating1 == "4":
+        x = int(rating1)
+    if rating1 == "5":
+        x = int(rating1)
+    else:
+        error_Message = " {} is not an integer since you can't follow directions you don't get to play "
+        print(error_Message.format(rating1))
+        x = int(3000)
+
+
+while 2999 > x > 5 or x < 1:
+    rating2 = input("please follow directions...From a scale of 1-5 how cute is this picture?: ")
     x = int(rating1)
 
 
@@ -21,12 +38,10 @@ except ValueError:
     error_Message = " {} is not an integer please only input integers "
     print(error_Message.format(rating2))
     rating2 = input("ok how cute is this one: ")
-    y = int(rating1)
+    y = int(rating2)
 while y > 5 or y < 1:
     rating2 = input("please follow directions...From a scale of 1-5 how cute is this one?: ")
-    y = int(rating1)
-
-
+    y = int(rating2)
 
 
 rating3 = input("Now what about this picture: ")
@@ -39,29 +54,32 @@ except ValueError:
     rating3 = input("Now what about this picture?: ")
 while z > 5 or z < 1:
     rating3 = input("please follow directions...From a scale of 1-5 what about this picture?: ")
-    z = int(rating1)
+    z = int(rating3)
 
 
 # this is the algorithm grading the users rating of the pictures
 def grading():
 
-    if x + y + z >= 13:
+    if x + y + z >= 3000:
+        print("thought you beat the system huh.... Nope I just wasted your time you don't get a grade")
+
+    if 2999 > x + y + z >= 13:
         print("Wow! You thought these pics were A+ , glad you liked them so much!")
         return
 
-    if x + y + z >= 12:
+    if 2999 > x + y + z >= 12:
         print("Glad you liked them! You gave these pics a B")
         return
 
-    if x + y + z >= 10:
+    if 2999 > x + y + z >= 10:
         print("huh guess these pictures were just ok, you gave them a C")
         return
 
-    if x + y + z >= 9:
+    if 2999 > x + y + z >= 9:
         print("oh geez not a fan i guess... you gave these pictures an D")
         return
 
-    else:
+    if x + y + z < 9:
         print("you didn't really like these did you... these pictures didn't pass ")
 
 
